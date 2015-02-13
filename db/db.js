c7d01ab6b4e94c5a4c92470744f9e613ee5e7d42
David Ernst <git@dsernst.com>
2015-02-12 17:07:38 -0800

    (feat) Working email intercepter

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var connectionString = process.env.MONGODB;
-mongoose.connect(connectionString); // connect to mongo database named shortly
+mongoose.connect(connectionString);
 
 var db = mongoose.connect;
 module.exports = db;
