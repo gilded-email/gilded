7e2fd1f78baf8d9b399a362862bfe31822f6c9c0
Neil Lokare <neil.lokare@gmail.com>
2015-02-12 19:47:23 -0800

    (tests) Adds tests framework

---
+++ test/tests.js
@@ -0,0 +1,82 @@
+var mocha = require('mocha');
+var assert = require('chai').assert;
+
+describe('User Module', function () {
+  it('should be able to sign up users', function () {
+    assert.equal(false, true);
+  });
+
+  it('should be able to sign in users', function () {
+    assert.equal(false, true);
+  });
+
+  it('should be able to add VIPs', function () {
+    assert.equal(false, true);
+  });
+
+  it('should be able to check VIP List', function () {
+    assert.equal(false, true);
+  });
+});
+
+describe('Email Module', function () {
+  it('should be able to receive emails', function () {
+    assert.equal(false, true);
+  });
+
+  it('should be able to send emails', function () {
+    assert.equal(false, true);
+  });
+
+  it('should be able to store emails', function () {
+    assert.equal(false, true);
+  });
+
+  it('should be able to retrieve emails from escrow', function () {
+    assert.equal(false, true);
+  });
+
+  it('should be able to remove emails from escrow', function () {
+    assert.equal(false, true);
+  });
+});
+
+describe('Payments Module', function () {
+  it('should be able to receive payments', function () {
+    assert.equal(false, true);
+  });
+
+  it('should be able to calculate total earnings', function () {
+    assert.equal(false, true);
+  });
+
+  it('should be able to calculate current balance', function () {
+    assert.equal(false, true);
+  });
+
+  it('should be able to remit payments', function () {
+    assert.equal(false, true);
+  });
+});
+
+describe('Settings Module', function () {
+  it('should be able to change forwarding email address', function () {
+    assert.equal(false, true);
+  });
+
+  it('should be able to change password', function () {
+    assert.equal(false, true);
+  });
+
+  it('should be able to add payment info', function () {
+    assert.equal(false, true);
+  });
+
+  it('should be able to remove payment info', function () {
+    assert.equal(false, true);
+  });
+
+  it('should confirm a payment has been received', function () {
+    assert.equal(false, true);
+  });
+});
