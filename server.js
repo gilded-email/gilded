05e370b99eb1ba9c8bd9a661980a905014f1d094
Neil Lokare <neil.lokare@gmail.com>
2015-02-13 17:28:31 -0800

    (deploy) Changes to package.json for deployment and a few minor refactors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var marketing = require('./server/marketing/marketingController.js');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.set('host', process.env.HOST || 'localhost');
+app.set('host', process.env.HOST || '127.0.0.1');
 
 app.use('/', express.static(path.join(__dirname, 'dist')));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,10 +18,8 @@ app.listen(app.get('port'));
 console.log('Server is listening on ' + app.get('host') + ':' + app.get('port'));
 
 app.use('/inbound', email.receive, email.verify); //handle all emails to application domain;
+app.use('/pay/:id', email.release); //TODO: payments module will handle Stripe transactions and will need to run first
 
 app.post('/signup', function (req, res) {
-  // store email in database
   marketing.addSignup(req, res);
 });
-
-app.use('/pay/:id', email.release); //TODO: payments module will handle Stripe transactions and will need to run first
