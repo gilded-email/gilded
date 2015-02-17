17506889efbb35ca7882e1bc7015a3a43faa1ab1
Christian Reyes <csreyes@berkeley.edu>
2015-02-17 14:34:49 -0800

    (feat) add history, vip, and settings components

---
+++ client/js/components/settings.js
@@ -0,0 +1,16 @@
+/**
+ * @jsx React.DOM
+ */
+var React = require('react');
+var mui = require('material-ui');
+var Actions = require('../actions/actions');
+
+var Settings = React.createClass({
+  render: function() {
+    return (
+      <div className="Settings">caasdfasdfasdfasdt</div>
+    );
+  }
+});
+
+module.exports = Settings;
---
+++ client/js/routes/routes.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var Router = require('react-router');
+var Route = Router.Route;
+var Redirect = Router.Redirect;
+var DefaultRoute = Router.DefaultRoute;
+
+var Routes = ();
+
+module.exports = Routes;
