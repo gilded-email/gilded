67c89782d7a191e06e8fa36a2d248b7b1df93472
Christian Reyes <csreyes@berkeley.edu>
2015-02-16 19:50:19 -0800

    (feat) working on dashboard components

--- client/js/components/app.js
+++ client/js/components/app.js
@@ -4,8 +4,10 @@
 var React = require('react');
 var Signup = require('./signup');
 var Login = require('./login');
+var Dashboard = require('./dashboard');
 var Router = require('react-router');
 var Route = Router.Route;
+var History = require('./history');
 var RouteHandler = Router.RouteHandler;
 var Link = Router.Link;
 var App = React.createClass({
@@ -15,6 +17,7 @@ var App = React.createClass({
 			<div>
 				<li><Link to="signup">Sign up</Link></li>
 				<li><Link to="login">Login</Link></li>
+        <li><Link to="dashboard">Dashboard</Link></li>
 			  <RouteHandler />
 			</div>
 		)
@@ -26,6 +29,8 @@ var routes = (
   <Route handler={App}>
     <Route name="signup" handler={Signup}/>
     <Route name="login" handler={Login}/>
+    <Route name="dashboard" handler={Dashboard}/>
+    <Route name="history" handler={History}/>
   </Route>
 );
 
