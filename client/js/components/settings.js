7a0688cba870bae0aba227e323869a4377b85912
Christian Reyes <csreyes@berkeley.edu>
2015-02-20 15:21:58 -0800

    (feat) refactor settings page

--- client/js/components/settings.js
+++ client/js/components/settings.js
@@ -2,13 +2,14 @@
  * @jsx React.DOM
  */
 var React = require('react');
-var mui = require('material-ui');
-var TextField = mui.TextField;
-var RaisedButton = mui.RaisedButton;
 var Actions = require('../actions/actions');
 var StoreWatchMixin = require('../mixins/StoreWatchMixin');
 var Store = require('../stores/store');
-
+var mui = require('material-ui');
+var TextField = mui.TextField;
+var RaisedButton = mui.RaisedButton;
+var Paper = mui.Paper;
+var dollarString = require('dollar-string');
 
 var getInitialState = function() {
   return null;
@@ -17,6 +18,17 @@ var getInitialState = function() {
 var Settings = React.createClass({
 
   mixins: [StoreWatchMixin(getInitialState)],
+
+  getDefaultProps: function() {
+      return {
+        settings: {
+          forwardEmail: "",
+          balance: 0,
+          rate: 0,
+          password: ""
+        }
+      };
+    },
 
   changeForwardEmail: function() {
     var newForwardEmail = this.refs.newEmail.getValue();
@@ -30,36 +42,62 @@ var Settings = React.createClass({
     Actions.updatePassword(newPassword);
   },
 
+  getRate: function() {
+    var rate = this.props.settings.rate;
+    if (rate) {
+      return dollarString.fromCents(rate);
+    } else {
+      return 0;
+    }
+  },
+
   changeRate: function() {
     var newRate = this.refs.newRate.getValue();
+    var newRateInCents = dollarString.toCents(newRate);
     this.refs.newRate.setValue('');
-    Actions.updateRate(newRate);
+    Actions.updateRate(newRateInCents);
+  },
+
+  getBalance: function() {
+    var balance = this.props.settings.balance;
+    return dollarString.fromCents(0);
   },
 
   render: function() {
     return (
-      <div className="Settings">
         <div className="dashboard">
-          <h1>Settings</h1>
-          <h2>Change Forwarding Email</h2>
-            <span>{this.props.settings.forwardEmail}</span>
-            <TextField ref="newEmail" floatingLabelText="New Email"/>
-            <RaisedButton label="Save" secondary={true} onClick={this.changeForwardEmail}/>
-          <h2>Change Password</h2>
-            <span>{this.props.settings.password}</span>
-            <TextField ref="newPassword" floatingLabelText="New Password"/>
-            <RaisedButton label="Save" secondary={true} onClick={this.changePassword}/>
-          <h2>Change Rate</h2>
-            <span>{this.props.settings.rate}</span>
-            <TextField ref="newRate" floatingLabelText="New Rate" />
-            <RaisedButton label="Save" secondary={true} onClick={this.changeRate}/>
-          <h2>Current Balance</h2>
-            <div className="balance">
-              ${this.props.settings.balance}
-            </div>
+          <div className="dashboard-title">
+            <h1>Account Settings</h1>
+          </div>
+          <div className="dashboard-subcontent">
+            <div className="dashboard-subheading">Forward Email</div>
+              <div className="dashboard-subheading-content">
+                <TextField ref="newEmail" className="new-email" floatingLabelText={this.props.settings.forwardEmail} />
+                <RaisedButton className="new-email-save" label="Update Email" secondary={true} onClick={this.changeForwardEmail} />
+              </div>
+          </div>
+          <div className="dashboard-subcontent">
+            <div className="dashboard-subheading">Password</div>
+              <div className="dashboard-subheading-content">
+                <TextField ref="newPassword" className="new-password" floatingLabelText="Enter new password" />
+                <RaisedButton className="new-password-save" label="Change Password" secondary={true} onClick={this.changePassword} />
+              </div>
+          </div>
+          <div className="dashboard-subcontent">
+            <div className="dashboard-subheading">Update Rate</div>
+              <div className="dashboard-subheading-content">
+                <TextField ref="newRate" className="new-rate" hintText="$" floatingLabelText={this.getRate()} />
+                <RaisedButton className="new-rate-save" label="Change Rate" secondary={true} onClick={this.changeRate} />
+              </div>
+          </div>
+          <div className="dashboard-subcontent">
+            <div className="dashboard-subheading">Current Balance</div>
+              <div className="dashboard-subheading-content">
+                {this.getBalance()}
+              </div>
+          </div>
         </div>
-      </div>
-    );
+    )
   }
 });
 
