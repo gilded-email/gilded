943668a07d44ae1c11bf6b30cf4e7e76c5cb3c6a
bcbcb <bcbcb@users.noreply.github.com>
2015-02-21 17:23:45 -0800

    (feat) Remove fake data from server, email front end WIP

--- client/js/components/emails.js
+++ client/js/components/emails.js
@@ -13,14 +13,33 @@ var Actions = require('../actions/actions');
 var Store = require('../stores/store');
 var StoreWatchMixin = require('../mixins/StoreWatchMixin');
 
+
+/**
+[
+  {"_id":"54e67b9b65379647526ddc60","email":"{\"to\":[\"tests@g.mtm.gs\"],\"from\":\"gildedtest@dsernst.com\",\"subject\":\"Test Email\",\"html\":\"<h1>Testing</h1>\",\"text\":\"Testing\"}","recipient":"tests","__v":0,"cost":100,"paid":false,"sentDate":"2015-02-20T00:11:07.123Z"},
+  {"_id":"54e67b9be60a837653ab668f","email":"{\"envelope\":\"{\\\"to\\\":[\\\"tests@g.mtm.gs\\\"]}\",\"from\":\"Tester Guy <gildedtest@dsernst.com>\",\"subject\":\"Test Email\",\"html\":\"<h1>Testing</h1>\",\"text\":\"Testing\"}","recipient":"tests","__v":0,"cost":500,"paid":false,"sentDate":"2015-02-20T00:11:07.359Z"}
+]
+**/
+
+var fakeData = [
+  {"_id":"54e67b9b65379647526ddc60","email":"{\"to\":[\"tests@g.mtm.gs\"],\"from\":\"gildedtest@dsernst.com\",\"subject\":\"Test Email\",\"html\":\"<h1>Testing</h1>\",\"text\":\"Testing\"}","recipient":"tests","__v":0,"cost":100,"paid":false,"sentDate":"2015-02-20T00:11:07.123Z"},
+  {"_id":"54e67b9be60a837653ab668f","email":"{\"envelope\":\"{\\\"to\\\":[\\\"tests@g.mtm.gs\\\"]}\",\"from\":\"Tester Guy <gildedtest@dsernst.com>\",\"subject\":\"Test Email\",\"html\":\"<h1>Testing</h1>\",\"text\":\"Testing\"}","recipient":"tests","__v":0,"cost":500,"paid":false,"sentDate":"2015-02-20T00:11:07.359Z"}
+]
+
 var getInitialState = function() {
-  var data = Store.getUserEmails();
+  // var data = [];
+  // data = Store.getUserEmails();
   return {
-    escrow: data
+    escrow: fakeData
   };
 };
 
 var Escrows = React.createClass({
+  emailData: function () {
+      if(!this.props.data.map) return [];
+      else return this.props.data;
+  },
+
   render: function () {
     return (
       <table>
@@ -34,16 +53,17 @@ var Escrows = React.createClass({
           </tr>
         </thead>
         <tbody>
-          {this.props.data.map(function (escrow, i) {
-            var email = JSON.parse(escrow.email);
-            email.index = i + 1;
-            email.paid = escrow.paid ? "Paid" : "Unpaid";
-            email.cost = dollarString.fromCents(escrow.cost);
-            email.sentDate = moment(escrow.sentDate).format('MMM DD');
-            return (
-              <Email {...email} key={i} />
-              )
-          })}
+          { this.emailData().map(function (escrow, i) {
+          var email = JSON.parse(escrow.email);
+          email.emailId = i;
+          email.paid = escrow.paid ? "Paid" : "Unpaid";
+          email.cost = dollarString.fromCents(escrow.cost);
+          email.sentDate = moment(escrow.sentDate).format('MMM DD');
+          return (
+            <Email {...email} key={i} />
+          )
+        }
+      )}
         </tbody>
       </table>
       )
@@ -51,9 +71,13 @@ var Escrows = React.createClass({
 });
 
 var Email = React.createClass({
+  mixins: [Router.Navigation],
+  viewEmail: function () {
+    this.transitionTo('email', {emailId: this.props.emailId});
+  },
   render: function () {
     return (
-      <tr>
+      <tr onClick={this.viewEmail}>
         <td>{this.props.from}</td>
         <td>{this.props.subject}</td>
         <td>{this.props.sentDate}</td>
@@ -68,14 +92,13 @@ var Emails = React.createClass({
   mixins: [StoreWatchMixin(getInitialState)],
 
   render: function () {
-
     return (
       <div className="emails">
 
         <div className="dashboard">
           <h1>Emails</h1>
 
-          <Escrows data={this.props.escrow} />
+          <Escrows data={fakeData} />
 
         </div>
       </div>
